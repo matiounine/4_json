1f5ab46a5d5b59302d684488213797b490cda256
Vladimir Matyunin <matiounine@mail.ru>
2018-01-23 03:01:07 +0300

    исправлены имена и комментарии

--- pprint_json.py
+++ pprint_json.py
@@ -4,50 +4,45 @@ from os import remove
 from urllib.request import urlretrieve
 
 
-def usg_msg_err():
-    """Return usage reference and error boilerplate."""
-    usg = "python.exe pprint_json.py [path][*.json]"
-    err = "pprint_json.py: error: "
-    return "Usage: " + usg + "\n\n" + err
+def usage_and_error():
+    usage_msg = "python.exe pprint_json.py [path][.json]"
+    error_stub = "pprint_json.py: error: "
+    return "Usage: " + usage_msg + "\n\n" + error_stub
 
 
-def load_data(data):
-    """Parse a *.json file sent as an argument."""
-    f = open(data, encoding="UTF-8")
-    res = json.load(f)
-    f.close()
-    return res
+def load_data(json_to_decode):
+    file_obj = open(json_to_decode, encoding="UTF-8")
+    json_decoded = json.load(file_obj)
+    file_obj.close()
+    return json_decoded
 
 
-def pretty_print_json(data):
-    """Re-format data from a *.json file
-    and send the result to a new file and to the console.
-    """
-    s = json.dumps(load_data(data),
-                   ensure_ascii=False, sort_keys=True, indent=4)
-    f = open("pretty.json", "w")
-    f.write(s)
-    f.close()
+def pretty_print_json(json_to_format):
+    json_formatted = json.dumps(load_data(json_to_format),
+                                ensure_ascii=False, sort_keys=True, indent=4)
+    new_json_file = open("pretty.json", "w")
+    new_json_file.write(json_formatted)
+    new_json_file.close()
     try:
-        print(s)
+        print(json_formatted)
     except UnicodeEncodeError:
         print("Cannot print to console")
-        print(usg_msg_err() + "data not supported by code page")
+        print(usage_and_error() + "data not supported by code page")
 
 
 if __name__ == "__main__":
     try:
-        if len(sys.argv) > 1:  # handle a command line input
+        if len(sys.argv) > 1:
             sys.argv[1] = sys.argv[1].replace("'", "")
             if sys.argv[1].find("http:") == 0\
                     or sys.argv[1].find("https:") == 0:
-                file = "temp.json"  # a temporary file to store a web *.json
-                urlretrieve(sys.argv[1], file)
-                pretty_print_json(file)
-                remove(file)
+                tmp_json_file = "temp.json"
+                urlretrieve(sys.argv[1], tmp_json_file)
+                pretty_print_json(tmp_json_file)
+                remove(tmp_json_file)
             else:
                 pretty_print_json(sys.argv[1])
         else:
-            print(usg_msg_err() + "no file or path provided")
+            print(usage_and_error() + "no file or path provided")
     except OSError:
-        print(usg_msg_err() + "invalid *.json file or path")
+        print(usage_and_error() + "invalid .json file or path")
